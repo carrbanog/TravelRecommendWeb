676e41381b9f6cac93e27b71a5a03b0d3a7d3dee
carrbanog <loopang5954@gmail.com>
2025-10-01 23:56:22 +0900

    페이지 이동시 마커 위치로 이동

--- src/entities/selected-place/model/selectedPlacesStore.ts
+++ src/entities/selected-place/model/selectedPlacesStore.ts
@@ -1,8 +1,11 @@
 import { create } from 'zustand';
 import type { NearPlace } from '../../../shared/types/nearPlaceType';
+import type { coordinates } from '../../../shared/types/coordinatestype';
 
 type SelectedPlacesState = {
   selectedPlaces: NearPlace[];
+  lastCoords?: coordinates;
+  setLastCoords: (coords: coordinates) => void;
   addPlace: (place: NearPlace) => void;
   removePlace: (title: string) => void;
   clear: () => void;
@@ -11,6 +14,7 @@ type SelectedPlacesState = {
 
 export const useSelectedPlacesStore = create<SelectedPlacesState>((set) => ({
   selectedPlaces: [],
+  lastCoords: undefined,
   addPlace: (place) => 
     set((state) => {
       if(state.selectedPlaces.find((p) => p.title === place.title)){
@@ -23,6 +27,6 @@ export const useSelectedPlacesStore = create<SelectedPlacesState>((set) => ({
     set((state) => ({
       selectedPlaces: state.selectedPlaces.filter((p) => p.title !== title),
     })),
-
+  setLastCoords: (coords) => set({lastCoords: coords}),
   clear: () => set({selectedPlaces: []})
 }))
--- src/pages/TravelPage/index.tsx
+++ src/pages/TravelPage/index.tsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
-import { LoadScript, LoadScriptNext } from '@react-google-maps/api';
+import { LoadScript, LoadScriptNext } from "@react-google-maps/api";
 
 import SearchForm from "../../features/travel/search-place/ui/SearchForm";
 import MyMap from "../../shared/ui/GoogleMap/MyMap";
-import SelectedList from '../../entities/selected-place/ui/SelectedList';
+import SelectedList from "../../entities/selected-place/ui/SelectedList";
 
 import { useGeocodeQuery } from "../../features/travel/search-place/hooks/useGeoCodeQuery";
 import { useNearcodeQuery } from "../../features/travel/near-place/hooks/useNearcodeQuery";
-import { useSelectedPlacesStore } from '../../entities/selected-place/model/selectedPlacesStore';
+import { useSelectedPlacesStore } from "../../entities/selected-place/model/selectedPlacesStore";
 
 export const TravelPage = () => {
   const [placeSearch, setPlaceSearch] = useState<string>("");
   // const [selected, setSelected] = useState<NearPlace>()
 
-  const { data: coords, isLoading: loadingCoords } = useGeocodeQuery(placeSearch); //검색 시 좌표 반환
-  const { data: nearPlaces, isLoading: loadingPlaces } = useNearcodeQuery(coords); //여행지 검색 시 주변 여행지 출력
+  const { data: coords, isLoading: loadingCoords } =
+    useGeocodeQuery(placeSearch); //검색 시 좌표 반환
+  const { data: nearPlaces, isLoading: loadingPlaces } =
+    useNearcodeQuery(coords); //여행지 검색 시 주변 여행지 출력
   // console.log(nearPlaces);
 
   const selectedPlaces = useSelectedPlacesStore((s) => s.selectedPlaces);
   const addPlace = useSelectedPlacesStore((s) => s.addPlace);
   // const removePlace = useSelectedPlacesStore((s) => s.removePlace);
+
+  //마지막 위치로 이동
+  // const setLastCoords = useSelectedPlacesStore((s) => s.setLastCoords);
+  // const lastCoords = useSelectedPlacesStore((s) => s.lastCoords);
+  // const mapCenter = lastCoords ?? coords;
 
   //주변 관광지 추천
 
@@ -33,15 +40,22 @@ export const TravelPage = () => {
         <div className="w-[70%] rounded-lg overflow-hidden shadow-md">
           {loadingCoords && <div>지도 로딩 중...</div>}
           <LoadScriptNext googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAP}>
-            <MyMap place={coords} nearPlaces={nearPlaces} onAddPlace={addPlace} />
+            <MyMap
+              place={coords}
+              nearPlaces={nearPlaces}
+              // onAddPlace={(place) => {
+              //   addPlace(place);
+              //   setLastCoords(place.nearCoordinates); // 클릭 시 좌표 저장
+              // }}
+              onAddPlace={addPlace}
+            />
           </LoadScriptNext>
-          
         </div>
 
         {/* 검색창 영역 (30%) */}
         <div className="w-[30%] flex flex-col gap-4">
           <SearchForm setPlaceSearch={setPlaceSearch} />
-          <SelectedList place={selectedPlaces}/>
+          <SelectedList place={selectedPlaces} />
         </div>
       </main>
     </div>
