5574c9f7378eb71d476e5b460ded381bdf5f1129
carrbanog <loopang5954@gmail.com>
2025-11-21 10:58:54 +0900

    제네릭을 통해서 공통 post요청 생성

--- src/entities/post/api/postApi.ts
+++ src/entities/post/api/postApi.ts
@@ -1,19 +1,33 @@
-
-import api from '../../../shared/api/axiosInstance'
-import type { CreatePostResponse, CreatePost, Post } from '../model/postTypes';
-
+import { apiClient } from "../../../shared/api/apiClient";
+import api from "../../../shared/api/axiosInstance";
+import type { CreatePostResponse, CreatePost, Post } from "../model/postTypes";
 
 export const fetchPosts = async () => {
-  const response = await api.get('/posts');
+  const response = await api.get("/posts");
   return response.data;
-}
+};
 
 export const fetchPostById = async (id: string): Promise<Post> => {
   const response = await api.get(`posts/${id}`);
-  return response.data
-}
-
-export const createPostApi = async (post:CreatePost):Promise<CreatePostResponse> => {
-  const response = await api.post<CreatePostResponse>('/createpost', post);
+  // console.log(response)
   return response.data;
-}
+};
+
+export const createPostApi = async (
+  post: CreatePost
+): Promise<CreatePostResponse> => {
+  const response = await api.post<CreatePostResponse>("/createpost", post);
+  console.log(response);
+  return response.data;
+};
+
+export const createPost = async (
+  post: CreatePost
+): Promise<CreatePostResponse | undefined> => {
+  if (!post) return;
+  const res = await apiClient.post<CreatePost, CreatePostResponse>(
+    "/createpost1",
+    post
+  );
+  return res;
+};
--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -1,7 +1,8 @@
 // 이 파일서 모든 get, post요청을 관리합니다.
-import api from "./axiosInstance"
-import axios, { AxiosError } from "axios";
+import api from "./axiosInstance";
+import axios, { Axios, AxiosError } from "axios";
 import type { AxiosResponse, AxiosRequestConfig } from "axios";
+import { data } from "react-router-dom";
 
 const handleApiError = (error: AxiosError | Error) => {
   if (axios.isAxiosError(error)) {
@@ -17,6 +18,20 @@ export const apiClient = {
     try {
       const response = await api.get<T>(url, config);
       console.log("get요청", response);
+      return response.data;
+    } catch (error) {
+      handleApiError(error as AxiosError | Error);
+      throw error;
+    }
+  },
+
+  post: async <TRequest, TResponse>(
+    url: string,
+    data: TRequest,
+    config?: AxiosRequestConfig
+  ): Promise<TResponse> => {
+    try {
+      const response = await api.post<TResponse>(url, data, config);
       return response.data;
     } catch (error) {
       handleApiError(error as AxiosError | Error);
