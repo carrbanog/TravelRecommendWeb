00e0483cfc8fb960faa5bcbb126a55b2704a98c9
carrbanog <loopang5954@gmail.com>
2025-11-21 13:00:12 +0900

    login api 수정

--- src/features/auth/login/api/loginApi.ts
+++ src/features/auth/login/api/loginApi.ts
@@ -2,9 +2,18 @@ import type { LoginResponse } from "../../login/model/type";
 import type { LoginData } from "../../../../shared/types/usertype";
 import api from "../../../../shared/api/axiosInstance";
 import { AUTH_ENDPOINTS } from "../../../../shared/api/endpoints";
+import { apiClient } from "../../../../shared/api/apiClient";
+
+// export const loginApi = async (data: LoginData): Promise<LoginResponse> => {
+//   const response = await api.post<LoginResponse>(AUTH_ENDPOINTS.LOGIN, data);
+//   console.log(response);
+//   return response.data;
+// };
 
 export const loginApi = async (data: LoginData): Promise<LoginResponse> => {
-  const response = await api.post<LoginResponse>(AUTH_ENDPOINTS.LOGIN, data);
-  console.log(response);
-  return response.data;
+  const res = await apiClient.post<LoginData, LoginResponse>(
+    AUTH_ENDPOINTS.LOGIN,
+    data
+  );
+  return res;
 };
