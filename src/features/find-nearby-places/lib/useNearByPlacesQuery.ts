f29a4a7dc90303abe793816bcc015d84698f9431
carrbanog <loopang5954@gmail.com>
2026-05-30 22:20:57 +0900

    query문에 에러 추가

--- src/features/find-nearby-places/lib/useNearByPlacesQuery.ts
+++ src/features/find-nearby-places/lib/useNearByPlacesQuery.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchNearbyPlaces } from "../../../entities/place/api/placeApi"
-import { data } from "react-router-dom";
-import type { coordinates } from "../../../shared/types/coordinatestype";
+import { toast } from "sonner"
 import type { NearbyPlaceParams } from "../../../entities/place/model/type"
 
 export const useNearcodeQuery = ({coords, type}: NearbyPlaceParams) => {
@@ -10,5 +9,14 @@ export const useNearcodeQuery = ({coords, type}: NearbyPlaceParams) => {
     queryKey: ["nearbyPlaces", coords, type],
     queryFn: () => fetchNearbyPlaces({coords, type}),
     enabled: !!coords,
+    meta:{
+      onError: (error:any) => {
+        console.error("🚨 주변 장소 조회 중 에러 발생:", error);
+
+        toast.error("장소 조회 실패 ❌", {
+          description: error.customMessage || "주변 여행지를 불러오는 중 오류가 발생했습니다.",
+        });
+      }
+    }
   });
 };
--- src/features/search-plcae/lib/useGeoCodeQuery.ts
+++ src/features/search-plcae/lib/useGeoCodeQuery.ts
@@ -1,11 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchMapCode } from "../../../entities/place/api/placeApi"
+import { fetchMapCode } from "../../../entities/place/api/placeApi";
+import { toast } from "sonner";
 
 export const useGeocodeQuery = (placeSearch: string) => {
   return useQuery({
     queryKey: ["geocode", placeSearch],
     queryFn: () => fetchMapCode(placeSearch),
     enabled: !!placeSearch, //빈 문자열이면 api 호출 막음
-    select: (data) => data?.geometry.location, //원본 데이터를 원하는 형태로 가공
+    // select: (data) => data?.geometry.location, 
+    //원본 데이터를 원하는 형태로 가공
+    retry: false, // 지역을 검색할 때는 실패 시 재시도하지 않도록 설정(너무 오래 걸림)
+    meta: {
+      onError: (error: any) => {
+        console.error("🚨 장소 검색 중 에러 발생:", error);
+        toast.error("장소 검색 실패 ❌", {
+          description:
+            error.customMessage ||
+            "장소를 검색하는 중 오류가 발생했습니다.",
+        });
+      },
+    },
   });
 };
