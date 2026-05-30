6744f105db5e4c7d718787e7fc7c575471738677
carrbanog <loopang5954@gmail.com>
2026-05-30 22:22:22 +0900

    전역에서 관리하는 에러문으로 수정 기존에는 console만 출력

--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -3,13 +3,27 @@ import api from "./axiosInstance";
 import axios, { Axios, AxiosError } from "axios";
 import type { AxiosResponse, AxiosRequestConfig } from "axios";
 
-const handleApiError = (error: AxiosError | Error) => {
-  if (axios.isAxiosError(error)) {
-    console.error("API Client Error:", error.response?.data || error.message);
-    console.log("Error발생!!");
-  } else {
-    console.error("Unexpected Error:", error.message);
+export const handleApiError = (error: AxiosError | Error) => {
+  // 1. 기본 메시지 세팅
+  let errorMessage = "서버 요청 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.";
+
+  // 2. 만약 커스텀 에러(프론트에서 직접 던진 에러)라면?
+  if (error instanceof Error && !(axios.isAxiosError(error))) {
+    errorMessage = error.message;
+    console.error("[Custom Error]:", errorMessage);
+  } 
+  // 3. 만약 백엔드 통신 중 터진 'Axios 에러'라면?
+  else if (axios.isAxiosError(error)) {
+    if (error.response) {
+      errorMessage = error.response.data.message || errorMessage;
+    } else if (error.request) {
+      errorMessage = "서버와 연결할 수 없습니다. 인터넷 연결 상태를 확인해주세요.";
+    }
+    console.error("[Axios API Error]:", errorMessage);
   }
+
+  // 4. 최종적으로 정제된 메시지를 주머니에 주입하고 위로 토스!
+  (error as any).customMessage = errorMessage;
   throw error;
 };
 
