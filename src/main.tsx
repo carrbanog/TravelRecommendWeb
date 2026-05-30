06805a6b47eeb7246dbc072d608011af51ec9c9b
carrbanog <loopang5954@gmail.com>
2026-05-30 22:19:51 +0900

    react query 에러 문 메인에 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import "./app/styles/global.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// 💡 QueryCache를 함께 사용하기 위해 임포트에 추가합니다.
+import { QueryClient, QueryClientProvider, QueryCache } from "@tanstack/react-query";
 import "./index.css";
 
-const queryClient = new QueryClient();
+// ✨ 전역 에러 핸들링 허브가 장착된 QueryClient 설정
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      // useQuery들의 meta 속성에 onError가 등록되어 있다면 여기서 캐치해서 대신 실행해줍니다.
+      if (query.meta?.onError) {
+        (query.meta.onError as (error: any) => void)(error);
+      }
+    },
+  }),
+});
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
