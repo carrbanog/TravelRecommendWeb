cb03c3b38dcbe8036cb73ba3e5dffce2f372cd4a
carrbanog <loopang5954@gmail.com>
2026-06-01 22:07:50 +0900

    ㅇ

--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -5,21 +5,24 @@ import type { AxiosResponse, AxiosRequestConfig } from "axios";
 
 export const handleApiError = (error: AxiosError | Error) => {
   // 1. 기본 메시지 세팅
-  let errorMessage = "서버 요청 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.";
+  let errorMessage =
+    "서버 요청 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.";
 
-  // 2. 만약 커스텀 에러(프론트에서 직접 던진 에러)라면?
-  if (error instanceof Error && !(axios.isAxiosError(error))) {
-    errorMessage = error.message;
-    console.error("[Custom Error]:", errorMessage);
-  } 
-  // 3. 만약 백엔드 통신 중 터진 'Axios 에러'라면?
-  else if (axios.isAxiosError(error)) {
+  // 2. 만약 백엔드 통신 중 터진 'Axios 에러'라면?
+  if (axios.isAxiosError(error)) {
     if (error.response) {
       errorMessage = error.response.data.message || errorMessage;
     } else if (error.request) {
-      errorMessage = "서버와 연결할 수 없습니다. 인터넷 연결 상태를 확인해주세요.";
+      errorMessage =
+        "서버와 연결할 수 없습니다. 인터넷 연결 상태를 확인해주세요.";
     }
     console.error("[Axios API Error]:", errorMessage);
+  }
+
+  // 3. 만약 커스텀 에러(프론트에서 직접 던진 에러)라면?
+  else if (error instanceof Error && !axios.isAxiosError(error)) {
+    errorMessage = error.message;
+    console.error("[Custom Error]:", errorMessage);
   }
 
   // 4. 최종적으로 정제된 메시지를 주머니에 주입하고 위로 토스!
@@ -42,7 +45,7 @@ export const apiClient = {
   post: async <TRequest, TResponse>(
     url: string,
     data: TRequest,
-    config?: AxiosRequestConfig
+    config?: AxiosRequestConfig,
   ): Promise<TResponse> => {
     try {
       const response = await api.post<TResponse>(url, data, config);
@@ -55,7 +58,7 @@ export const apiClient = {
 
   delete: async <TResponse>(
     url: string,
-    config?: AxiosRequestConfig
+    config?: AxiosRequestConfig,
   ): Promise<TResponse> => {
     try {
       const response = await api.delete<TResponse>(url, config);
@@ -64,5 +67,5 @@ export const apiClient = {
       handleApiError(error as AxiosError | Error);
       throw error;
     }
-  }
+  },
 };
