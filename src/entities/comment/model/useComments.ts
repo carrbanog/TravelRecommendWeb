7309afaf7794018e275d4b73d450df5c65576096
carrbanog <loopang5954@gmail.com>
2026-07-02 15:41:08 +0900

    댓글 삭제할 때 댓글id가 일치하지 않아도 삭제되는 문제를 해결

--- src/entities/comment/model/useComments.ts
+++ src/entities/comment/model/useComments.ts
@@ -1,6 +1,8 @@
 import {useQuery, useMutation, useQueryClient} from "@tanstack/react-query";
 import type { Comment } from "./type";
 import { deleteCommentApi, fetchCommentsApi } from '../api/commentAPi';
+import { toast } from "sonner";
+
 
 // 특정 게시물의 댓글을 가져오는 API 함수
 
@@ -16,15 +18,16 @@ export const useComments = (postId: string) => {
 export const useDeleteComment = (postId: string, commentId: string) => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: () => deleteCommentApi(postId, commentId),
+    mutationFn: () => deleteCommentApi("postId", commentId), // 실제 commentId를 전달해야 함
     onSuccess: (data) => {
-      alert(data.message);
+      toast.success(data.message || "댓글이 성공적으로 삭제되었습니다.");
       queryClient.invalidateQueries({ queryKey: ['comments', postId] });
       // comments와 postID가 일치하는 쿼리를 무효화하여 댓글 목록을 최신 상태로 갱신
     },
-    onError: (error) => {
-      console.error("댓글 삭제 오류:", error);
-      alert("댓글 삭제 중 오류가 발생했습니다.");
+    onError: (error: any) => {
+      toast.error("댓글 삭제 중 오류가 발생했습니다.", {
+        description: error.customMessage || "잠시 후 다시 시도해보세요.",
+      });
     }
   });
 }
