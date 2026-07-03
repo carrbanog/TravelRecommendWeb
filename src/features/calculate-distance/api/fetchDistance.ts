78c7c26797dcbac1b1b2777988b9161221b0b212
carrbanog <loopang5954@gmail.com>
2026-07-03 21:09:27 +0900

    경로 요청 시 캐시를 사용해서 반복 요청 방지

--- src/features/calculate-distance/api/fetchDistance.ts
+++ src/features/calculate-distance/api/fetchDistance.ts
@@ -8,6 +8,8 @@ export const useFetchDistanceQuery = (locations: coordinates[]) => {
     queryKey: ["distance", locations],
     queryFn: () => fetchDistance(locations),
     enabled: locations && locations.length >= 2, // 위치 데이터가 있을 때만 쿼리 실행
+    staleTime: 5 * 60 * 1000, // 5분 동안은 데이터가 '신선(fresh)'하다고 판단하여 서버에 재요청하지 않고 캐시를 씁니다.
+    gcTime: 10 * 60 * 1000,
   });
 };
 
