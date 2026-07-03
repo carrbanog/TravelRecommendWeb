ef21acc7b620ac1ed9ecee959dbf71722b697040
carrbanog <loopang5954@gmail.com>
2026-07-03 12:41:04 +0900

    마우스 올리면 info 나오는 코드를 부모에서 props로 전달하도록 수정

--- src/features/travel-route/ui/TravelPathMapCanvas.tsx
+++ src/features/travel-route/ui/TravelPathMapCanvas.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Marker, Polyline, InfoWindow } from "@react-google-maps/api";
 import { useSelectedPlacesStore } from "@/entities/place/model/selectedPlacesStore";
 import { usePlaceDetailsQuery } from "@/entities/place/place-details/lib/usePlaceDetailsQuery";
-import { useMapHover } from "@/shared/lib/hooks/useMapHover";
 import { PlaceInfoWindow } from "@/entities/place/place-details/ui/PlaceInfoWindow";
 import MyMap from "@/shared/ui/GoogleMap/MyMap";
 import type { coordinates } from "@/shared/types/coordinatestype";
@@ -11,13 +10,15 @@ type Props = {
   activeTab: number;
   roadPath?: coordinates[];
   colors: string[];
+  hoveredPlace: string | null;
+  onPlaceHover: (placeId: string) => void;
+  onPlaceLeave: () => void;
 };
 
 // 사용자가 여행지 일정을 선택 후 경로를 보여주는 지도 컴포넌트
-export const TravelPathMapCanvas = React.memo(({ activeTab, roadPath, colors }: Props) => {
+export const TravelPathMapCanvas = React.memo(({ activeTab, roadPath, colors, hoveredPlace, onPlaceHover, onPlaceLeave }: Props) => {
   const center = useSelectedPlacesStore((s) => s.center);
   const selectedPlaces = useSelectedPlacesStore((s) => s.selectedPlaces);
-  const { hoveredPlace, handleMouseOver, handleMouseOut } = useMapHover(400);
   const { data: detailData, isLoading: detailLoading } = usePlaceDetailsQuery(hoveredPlace || "");
   console.log("TravelPathMapCanvas 렌더링", "activeTab:", activeTab, "roadPath:", roadPath, "colors:", colors)
   return (
@@ -27,8 +28,8 @@ export const TravelPathMapCanvas = React.memo(({ activeTab, roadPath, colors }: 
           key={place.placeId}
           position={place.nearCoordinates}
           title={place.title}
-          onMouseOver={() => handleMouseOver(place.placeId)}
-          onMouseOut={handleMouseOut}
+          onMouseOver={() => onPlaceHover(place.placeId)}
+          onMouseOut={onPlaceLeave}
         >
           {hoveredPlace === place.placeId && detailData && (
             <InfoWindow options={{ disableAutoPan: true }}>
