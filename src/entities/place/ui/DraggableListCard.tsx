32bb9840e62b118cf312e298e3bfba2f4dee3a39
carrbanog <loopang5954@gmail.com>
2026-07-05 00:27:39 +0900

    기존에 NearPlace 타입에서 pick해서 selectedPlace 타입을 사용하던걸 nearPlaceType폴더 내부에 선언

--- src/entities/place/ui/DraggableListCard.tsx
+++ src/entities/place/ui/DraggableListCard.tsx
@@ -1,8 +1,7 @@
 import { useDraggable } from "@dnd-kit/core";
-import type { NearPlace } from "../../../shared/types/nearPlaceType";
+import type { SelectedPlace } from "../../../shared/types/nearPlaceType";
 import { ListCardUI } from "./ListCardUI"; // 분리된 UI 컴포넌트 유지
 
-type SelectedPlace = Pick<NearPlace, "title" | "nearCoordinates" | "placeId">;
 
 type DraggableListCardProps = {
   place: SelectedPlace;
@@ -23,7 +22,7 @@ export const DraggableListCard = ({
       data: { place, from: "SelectedListCard" },
     });
 
-  console.log("DraggableListCard 렌더링", transform);
+  console.log("DraggableListCard 렌더링", place);
 
   // 드래그가 true로 변하면 undefined로 만들어서 기존 카드를 원래 자리에 고정
   const style = isDragging ? DRAGGING_STYLE : undefined;
--- src/shared/types/nearPlaceType.ts
+++ src/shared/types/nearPlaceType.ts
@@ -14,3 +14,5 @@ export type NearPlace = {
   type: string;
   // photos?: PhotoInfo[];
 };
+// 선택한 여행지를 근처 여행지와 동일한 타입으로 정의하여 일관성을 유지
+export type SelectedPlace = NearPlace;
