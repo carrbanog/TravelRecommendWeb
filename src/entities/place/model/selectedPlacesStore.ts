c9e623d18dae023f99ecf0360a9a9ccfc91964b2
carrbanog <loopang5954@gmail.com>
2026-07-05 00:25:22 +0900

    zustand에 여행지 말고 호텔전용 store도 추가(필요할경우 추가할 예정)

--- src/entities/place/model/selectedPlacesStore.ts
+++ src/entities/place/model/selectedPlacesStore.ts
@@ -1,34 +1,49 @@
-import { create } from 'zustand';
-import type { NearPlace } from '../../../shared/types/nearPlaceType';
-import type { coordinates } from '../../../shared/types/coordinatestype';
+import { create } from "zustand";
+import type { NearPlace } from "../../../shared/types/nearPlaceType";
+import type { coordinates } from "../../../shared/types/coordinatestype";
 
 type SelectedPlacesState = {
   selectedPlaces: NearPlace[];
+  selectedHotels: NearPlace[];
   lastCoords?: coordinates;
-  center: coordinates
-  setLastCoords: (coords: coordinates) => void;
+  center: coordinates;
   addPlace: (place: NearPlace) => void;
   removePlace: (place: string) => void;
+  addHotel: (hotel: NearPlace) => void;
+  removeHotel: (placeId: string) => void;
+  setLastCoords: (coords: coordinates) => void;
   setCenter: (coords: coordinates) => void;
-}
-
+};
 
 export const useSelectedPlacesStore = create<SelectedPlacesState>((set) => ({
   selectedPlaces: [],
+  selectedHotels: [],
   lastCoords: undefined,
   center: { lat: 37.5665, lng: 126.989 }, //지도 위치 중간값
-  addPlace: (place) => 
+  addPlace: (place) =>
     set((state) => {
-      if(state.selectedPlaces.find((p) => p.title === place.title)){
+      if (state.selectedPlaces.find((p) => p.title === place.title)) {
         return state;
       }
       // console.log(selectedPlaces)
-      return {selectedPlaces : [...state.selectedPlaces, place]}
+      return { selectedPlaces: [...state.selectedPlaces, place] };
     }),
   removePlace: (place) =>
     set((state) => ({
       selectedPlaces: state.selectedPlaces.filter((p) => p.placeId !== place),
     })),
-  setLastCoords: (coords) => set({lastCoords: coords}),
-  setCenter: (coords) => set({center:coords})
-}))
+
+  addHotel: (hotel) =>
+    set((state) => {
+      if (state.selectedHotels.find((h) => h.title === hotel.title))
+        return state;
+      return { selectedHotels: [...state.selectedHotels, hotel] };
+    }),
+  removeHotel: (placeId) =>
+    set((state) => ({
+      selectedHotels: state.selectedHotels.filter((h) => h.placeId !== placeId),
+    })),
+
+  setLastCoords: (coords) => set({ lastCoords: coords }),
+  setCenter: (coords) => set({ center: coords }),
+}));
