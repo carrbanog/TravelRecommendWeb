66e2e460bc5d441dd684ffe45d72d53783fcec78
carrbanog <loopang5954@gmail.com>
2026-07-16 14:45:55 +0900

    폴더 이름 오타  수정 search-plcae -> search-place

--- src/features/search-place/lib/useGeoCodeQuery.ts
+++ src/features/search-place/lib/useGeoCodeQuery.ts
@@ -6,10 +6,8 @@ export const useGeocodeQuery = (placeSearch: string) => {
   return useQuery({
     queryKey: ["geocode", placeSearch],
     queryFn: () => fetchMapCode(placeSearch),
-    enabled: !!placeSearch, //빈 문자열이면 api 호출 막음
-    // select: (data) => data?.geometry.location, 
-    //원본 데이터를 원하는 형태로 가공
-    retry: false, // 지역을 검색할 때는 실패 시 재시도하지 않도록 설정(너무 오래 걸림)
+    enabled: !!placeSearch,
+    retry: false, 
     meta: {
       onError: (error: any) => {
         console.error("🚨 장소 검색 중 에러 발생:", error);
